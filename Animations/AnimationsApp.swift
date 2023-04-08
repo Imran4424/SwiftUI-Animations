b614c7150a67aace5b2a92c0168f8ddcfb37c4b0
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-04-08 23:04:20 +0600

    project init

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Shah Md Imran Hossain on 8/4/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
