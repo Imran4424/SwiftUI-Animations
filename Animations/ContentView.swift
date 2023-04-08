e5b888eae7659c35cdae8d5cead8258c727cbb20
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-04-08 23:32:13 +0600

    ease in out repeat forever

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -23,7 +23,8 @@ struct ContentView: View {
 //        .animation(.default, value: animationAmount)
 //        .animation(.interpolatingSpring(stiffness: 50, damping: 1), value: animationAmount) // spring effect
 //        .animation(.easeInOut(duration: 2).delay(1), value: animationAmount)
-        .animation(.easeInOut(duration: 1).repeatCount(3, autoreverses: true), value: animationAmount)
+//        .animation(.easeInOut(duration: 1).repeatCount(3, autoreverses: true), value: animationAmount)
+        .animation(.easeInOut(duration: 1).repeatForever(autoreverses: true), value: animationAmount) // repeat forever
     }
 }
 
